12157698bed4c27ec2674a64013eaab8fc328e44
nikprosto <gnussum@gmail.com>
2022-01-28 00:55:35 +0300

    feat: partially add light theme

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,10 +1,12 @@
 const iconMenu = document.querySelector ('.menu-icon');
 const nav = document.querySelector ('.nav');
+const navLink = document.querySelectorAll ('.nav-link');
 if (iconMenu) {
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('lock');
         iconMenu.classList.toggle('active');
         nav.classList.toggle('active');
+        navLink.forEach(x => x.classList.toggle('active'));
     }
     )
 }
@@ -25,6 +27,31 @@ if (navLinks.length > 0) {
     }
 }
 
+const themeToggle = document.querySelector ('.theme-toggle');
+const html = document.querySelector ('.html');
+const skillTitle = document.querySelectorAll ('.skill-title');
+const skillText = document.querySelectorAll ('.skill-text');
+const sectionTitle = document.querySelectorAll ('.section-title');
+const sectionTitleStripe = document.querySelectorAll ('.section-title-stripe');
+const buttonSeasons = document.querySelectorAll ('.button');
+const priceCartName = document.querySelectorAll ('.price-cart-name');
+const priceCartText = document.querySelectorAll ('.price-cart-text');
+if (themeToggle) {
+    themeToggle.addEventListener("click", function(t) {
+        themeToggle.classList.toggle('light');
+        iconMenu.classList.toggle('light');
+        html.classList.toggle('light');
+        nav.classList.toggle('light');
+        navLink.forEach(x => x.classList.toggle('light'));
+        skillTitle.forEach(x => x.classList.toggle('light'));
+        skillText.forEach(x => x.classList.toggle('light'));
+        sectionTitle.forEach(x => x.classList.toggle('light'));
+        sectionTitleStripe.forEach(x => x.classList.toggle('light'));
+        buttonSeasons.forEach(x => x.classList.toggle('light'));
+        priceCartName.forEach(x => x.classList.toggle('light'));
+        priceCartText.forEach(x => x.classList.toggle('light'));
+    })
+}
 
 
 
