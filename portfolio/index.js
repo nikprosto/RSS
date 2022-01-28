83198f2f5d8a45addbf9442cd84d976d88e40015
nikprosto <gnussum@gmail.com>
2022-01-28 21:56:42 +0300

    feat: partially add translate

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,104 @@
+const i18Obj = {
+    'en': {
+      'skills': 'Skills',
+      'portfolio': 'Portfolio',
+      'video': 'Video',
+      'price': 'Price',
+      'contacts': 'Contacts',
+      'hero-title': 'Alexa Rise',
+      'hero-text': 'Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise',
+      'hire': 'Hire me',
+      'skill-title-1': 'Digital photography',
+      'skill-text-1': 'High-quality photos in the studio and on the nature',
+      'skill-title-2': 'Video shooting',
+      'skill-text-2': 'Capture your moments so that they always stay with you',
+      'skill-title-3': 'Rotouch',
+      'skill-text-3': 'I strive to make photography surpass reality',
+      'skill-title-4': 'Audio',
+      'skill-text-4': 'Professional sounds recording for video, advertising, portfolio',
+      'winter': 'Winter',
+      'spring': 'Spring',
+      'summer': 'Summer',
+      'autumn': 'Autumn',
+      'price-description-1-span-1': 'One location',
+      'price-description-1-span-2': '120 photos in color',
+      'price-description-1-span-3': '12 photos in retouch',
+      'price-description-1-span-4': 'Readiness 2-3 weeks',
+      'price-description-1-span-5': 'Make up, visage',
+      'price-description-2-span-1': 'One or two locations',
+      'price-description-2-span-2': '200 photos in color',
+      'price-description-2-span-3': '20 photos in retouch',
+      'price-description-2-span-4': 'Readiness 1-2 weeks',
+      'price-description-2-span-5': 'Make up, visage',
+      'price-description-3-span-1': 'Three locations or more',
+      'price-description-3-span-2': '300 photos in color',
+      'price-description-3-span-3': '50 photos in retouch',
+      'price-description-3-span-4': 'Readiness 1 week',
+      'price-description-3-span-5': 'Make up, visage, hairstyle',
+      'order': 'Order shooting',
+      'contact-me': 'Contact me',
+      'send-message': 'Send message'
+    },
+    'ru': {
+      'skills': 'Навыки',
+      'portfolio': 'Портфолио',
+      'video': 'Видео',
+      'price': 'Цены',
+      'contacts': 'Контакты',
+      'hero-title': 'Алекса Райс',
+      'hero-text': 'Сохраните искренние эмоции, романтические переживания и счастливые моменты жизни вместе с профессиональным фотографом',
+      'hire': 'Пригласить',
+      'skill-title-1': 'Фотография',
+      'skill-text-1': 'Высококачественные фото в студии и на природе',
+      'skill-title-2': 'Видеосъемка',
+      'skill-text-2': 'Запечатлите лучшие моменты, чтобы они всегда оставались с вами',
+      'skill-title-3': 'Ретушь',
+      'skill-text-3': 'Я стремлюсь к тому, чтобы фотография превосходила реальность',
+      'skill-title-4': 'Звук',
+      'skill-text-4': 'Профессиональная запись звука для видео, рекламы, портфолио',
+      'winter': 'Зима',
+      'spring': 'Весна',
+      'summer': 'Лето',
+      'autumn': 'Осень',
+      'price-description-1-span-1': 'Одна локация',
+      'price-description-1-span-2': '120 цветных фото',
+      'price-description-1-span-3': '12 отретушированных фото',
+      'price-description-1-span-4': 'Готовность через 2-3 недели',
+      'price-description-1-span-5': 'Макияж, визаж',
+      'price-description-2-span-1': 'Одна-две локации',
+      'price-description-2-span-2': '200 цветных фото',
+      'price-description-2-span-3': '20 отретушированных фото',
+      'price-description-2-span-4': 'Готовность через 1-2 недели',
+      'price-description-2-span-5': 'Макияж, визаж',
+      'price-description-3-span-1': 'Три локации и больше',
+      'price-description-3-span-2': '300 цветных фото',
+      'price-description-3-span-3': '50 отретушированных фото',
+      'price-description-3-span-4': 'Готовность через 1 неделю',
+      'price-description-3-span-5': 'Макияж, визаж, прическа',
+      'order': 'Заказать съемку',
+      'contact-me': 'Свяжитесь со мной',
+      'send-message': 'Отправить'
+    }
+  }
+const langToggle = document.querySelectorAll('.lang-toggle')
+if (langToggle) {
+    langToggle.forEach (x => x.addEventListener('click', function langToggleClick(e) {
+        const langToggleClick = e.target;
+        if (langToggle) {
+            langToggle.forEach(x => x.classList.remove('active'));
+            e.target.classList.add('active');
+        }
+    }))
+}
+
+function getTranslate(ru) {
+    const langToggleClick = e.target;
+        if (langToggle.classList.contains('active')) {
+            const translateElement = document.querySelectorAll ('[data-i18n]')
+            translateElement.forEach ()
+        }
+};
+
 const iconMenu = document.querySelector ('.menu-icon');
 const nav = document.querySelector ('.nav');
 const navLink = document.querySelectorAll ('.nav-link');
@@ -59,13 +160,6 @@ if (themeToggle) {
     })
 }
 
-// const button = document.querySelectorAll('.button');
-// if (button) {
-//     button.forEach (x => x.addEventListener('click', function(q) {
-//         button.forEach(x => x.classList.remove('active'))
-//     }))
-// }
-
 const button = document.querySelectorAll('.button');
 const portfolioImages = document.querySelectorAll('.photo')
 if (button) {
@@ -91,12 +185,3 @@ if (button) {
         }
     }
 }
-
-// if (button) {
-//     button.forEach (x => x.addEventListener('click', changeImage));
-
-//     function changeImage (e) {
-//         const buttonClick = e.target;
-        
-//     }
-// }
