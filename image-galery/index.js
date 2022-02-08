8cc710253cdb7fbc2a2b05820ffd47663841f7a0
nikprosto <gnussum@gmail.com>
2022-02-09 00:26:56 +0300

    feat: add all pictures

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,15 +1,29 @@
 const url = 'https://api.unsplash.com/search/photos?query=cats&per_page=30&orientation=landscape&client_id=04ufwLfYkUW_uO9OlQOojuE9hQFxR0veEPagGYh0VGA'
-// get data from API
-async function getData () {
-    const res = await fetch(url);
-    const data = await res.json();
-    const mainContainer = document.querySelector('.main-container');
-    // create img and insert it into main
-    const img = document.createElement('img');
-    img.classList.add('gallery-img');
-    img.src = `${data.results[0].urls.regular}`;
-    img.alt = 'image';
-    mainContainer.append(img);
+const main = document.querySelector('main');
+
+function getData() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showData(data.results);
+      });
+  }
+
+function showData(data) {
+    main.innerHTML = '';
+    data.forEach(photo => {
+        const {urls, alt_description} = photo;
+        const div = document.createElement('div');
+        div.classList.add('div-photo');
+        main.append(div);
+        const img = document.createElement('img');
+        img.classList.add('photo');
+        img.src = urls.regular;
+        img.alt = alt_description;
+        div.appendChild(img);
+    });
+
 }
 
 getData();
