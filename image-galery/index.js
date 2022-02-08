f36ba01f746dd8f02b3a36667bc94aed95c582f8
nikprosto <gnussum@gmail.com>
2022-02-08 16:24:16 +0300

    refactor: small changes

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,20 +1,15 @@
 const url = 'https://api.unsplash.com/search/photos?query=cats&per_page=30&orientation=landscape&client_id=04ufwLfYkUW_uO9OlQOojuE9hQFxR0veEPagGYh0VGA'
+// get data from API
 async function getData () {
     const res = await fetch(url);
     const data = await res.json();
     const mainContainer = document.querySelector('.main-container');
-const img = document.createElement('img');
-img.classList.add('gallery-img');
-img.src = `${data.results[0].urls.regular}`;
-img.alt = 'image';
-mainContainer.append(img);
-    console.log(data.results[0].urls.regular);
+    // create img and insert it into main
+    const img = document.createElement('img');
+    img.classList.add('gallery-img');
+    img.src = `${data.results[0].urls.regular}`;
+    img.alt = 'image';
+    mainContainer.append(img);
 }
-getData();
 
-// const mainContainer = document.querySelector('.main-container');
-// const img = document.createElement('img');
-// img.classList.add('gallery-img');
-// img.src = `${data.results[0].urls.regular}`;
-// img.alt = 'image';
-// mainContainer.append(img);
+getData();
